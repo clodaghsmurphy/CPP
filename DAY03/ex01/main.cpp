7fed136b39d41f074fea9fd8ae51dc91805e8f8e
clodaghsmurphy <clodagh9@yahoo.ie>
2022-08-25 11:16:09 +0100

    done ex01

--- DAY03/ex01/main.cpp
+++ DAY03/ex01/main.cpp
@@ -1,10 +1,36 @@
-# include "ClapTrap.hpp"
+#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
 
 int main()
 {
     ClapTrap clap("Mr.Clap");
-    FlagTrap flag("Mr.Flag");
+    ScavTrap scag("Mr.Scav");
+
+    std::cout << std::endl << "  SCAV STATS  " << std::endl;
 
     std::cout << clap.get_name() << std::endl;
-    std::cout << flag.get_name() << std::endl;
+    std::cout << clap.get_attack() << std::endl;
+    std::cout << clap.get_energy() << std::endl;
+    std::cout << clap.get_hpoints() << std::endl;
+
+
+    std::cout << std::endl << "  SCAV STATS  " << std::endl;
+
+    std::cout << scag.get_name() << std::endl;
+    std::cout << scag.get_attack() << std::endl;
+    std::cout << scag.get_energy() << std::endl;
+    std::cout << scag.get_hpoints() << std::endl;
+    std::cout << std::endl;
+
+    scag = ScavTrap("jones");
+    std::cout << std::endl
+              << "  SCAV STATS  " << std::endl;
+
+    std::cout << scag.get_name() << std::endl;
+    std::cout << scag.get_attack() << std::endl;
+    std::cout << scag.get_energy() << std::endl;
+    std::cout << scag.get_hpoints() << std::endl;
+    std::cout << std::endl;
+
+    scag.guardGate();
 }
