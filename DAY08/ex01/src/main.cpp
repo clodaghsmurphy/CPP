aae8b8a99917e629002acb2adaf80cb335b22959
Clodagh MURPHY <clmurphy@e1r8p11.clusters.42paris.fr>
2022-10-04 14:45:19 +0200

    ready to push

--- DAY08/ex01/src/main.cpp
+++ DAY08/ex01/src/main.cpp
@@ -86,6 +86,26 @@ void test5()
     }
 }
 
+void test6()
+{
+    try {
+        Span _span(50000);
+        std::vector<int>::iterator itb;
+        std::vector<int>::iterator ite;
+
+        itb = _span.getVec().begin();
+        ite = _span.getVec().end();
+        _span.addRange(itb, ite);
+        //_span.printVec();
+        std::cout << "\033[1m\033[34mSHORTEST SPAN : \033[0m\033[37m" << _span.shortestSpan() << std::endl;
+        std::cout << "\033[1m\033[34mLONGEST SPAN : \033[0m\033[37m" << _span.longestSpan() << std::endl;
+    }
+    catch (std::exception &e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+}
+
 
 int main()
 {
@@ -105,4 +125,7 @@ int main()
     std::cout <<  "\033[1m\033[35m TEST 5 🧪 \033[0m\033[37m" <<  std::endl;
     test5();
     std::cout << std::endl;  
+    std::cout <<  "\033[1m\033[35m TEST 6 🧪 \033[0m\033[37m" <<  std::endl;
+    test6();
+    std::cout << std::endl;
 }
--- DAY08/ex02/src/main.cpp
+++ DAY08/ex02/src/main.cpp
@@ -1,6 +1,6 @@
 # include "MutantStack.hpp"
-
-void PrintStack(MutantStack<int> s)
+template<class T>
+void PrintStack(T s)
 {
     if (s.empty())
         return;
@@ -13,37 +13,29 @@ void PrintStack(MutantStack<int> s)
     s.push(x);
 }
 
-void PrintStack_S(std::stack<int> s)
-{
-    if (s.empty())
-        return;
-     
- 
-    int x = s.top();
-    s.pop();
-    PrintStack_S(s);
-    std::cout << x << " ";
-    s.push(x);
-}
-
 void list_test()
 {
+    std::cout << std::endl << std::endl << "\033[1m\033[32m/////////////////////////////////////////";
+    std::cout << std::endl << "\033[1m\033[32m LIST TEST \033[0m\033[37m" ;
+    std::cout << std::endl << "\033[1m\033[32m/////////////////////////////////////////" << std::endl;
+    std::cout << std::endl;
     std::list<int> mstack;
-    mstack.push(5);
-    mstack.push(17);
-    std::cout << "\033[1m\033[33mAfter push 5, 17 Mutant stack top is : \033[0m\033[37m";
-    std::cout << mstack.top() << std::endl;
-    mstack.pop();
-    std::cout << "\033[1m\033[33mSize after first pop : \033[0m\033[37m";
+
+    mstack.push_back(5);
+    mstack.push_back(17);
+    std::cout << "\033[1m\033[33mAfter push_back 5, 17 Mutant stack top is : \033[0m\033[37m";
+    std::cout << mstack.back() << std::endl;
+    mstack.pop_back();
+    std::cout << "\033[1m\033[33mSize after first pop_back : \033[0m\033[37m";
     std::cout << mstack.size() << std::endl;
-    std::cout << "\033[1m\033[34m... Pushing numbers 3, 5, 737 and 0 ....\033[0m\033[37m" << std::endl;
-    mstack.push(3);
-    mstack.push(5);
-    mstack.push(737);
+    std::cout << "\033[1m\033[34m... Push_backing numbers 3, 5, 737 and 0 ....\033[0m\033[37m" << std::endl;
+    mstack.push_back(3);
+    mstack.push_back(5);
+    mstack.push_back(737);
     //[...]
-    mstack.push(0);
-    std::list::iterator it = mstack.begin();
-    std::list::iterator ite = mstack.end();
+    mstack.push_back(0);
+    std::list<int>::iterator it = mstack.begin();
+    std::list<int>::iterator ite = mstack.end();
     
     std::cout << "\033[1m\033[33mBefore increment iterator val : \033[0m\033[37m" << *it << std::endl;
     ++it;
@@ -87,9 +79,12 @@ int main()
         std::cout << *it << " ";
         ++it;
     }
-    std::cout << std::endl << "\033[1m\033[34mPritning copy of Mutantstackin stack form \033[0m\033[37m" << std::endl;
+    std::cout << std::endl << "\033[1m\033[34mPritning copy == of Mutantstack \033[0m\033[37m" << std::endl;
+    MutantStack<int> stack_copy = mstack;
+    PrintStack(stack_copy);
+    std::cout << std::endl << "\033[1m\033[34mPritning copy of Mutantstack in stack form \033[0m\033[37m" << std::endl;
     std::stack<int> s(mstack);
-    PrintStack_S(s);
+    PrintStack(s);
     list_test();
     return (0);
 }
